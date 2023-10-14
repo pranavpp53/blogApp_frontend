36d2e832a8f0393df340429b5d33aa93e37a301a
Pranav PP <pppranav53@gmail.com>
2023-10-14 08:18:39 +0530

    second_push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,83 +3,70 @@ import { Link, useNavigate } from 'react-router-dom'
 import './header.css'
 
 function Header() {
-  const [user, dispatch] = useState(false)
-  const navigate=useNavigate()
-  const [username,setusername]=useState('')
+  const [user, dispatch] = useState(false);
+  const [username, setusername] = useState('');
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  const handleToggleNav = () => {
+    setIsNavVisible(!isNavVisible);
+  };
 
   const handleLogout = () => {
     localStorage.removeItem("userId");
-    localStorage.removeItem("userName")
-    alert('logout successfull')
-    navigate("")
+    localStorage.removeItem("userName");
+    alert('Logout successful');
     window.location.reload();
   };
 
   useEffect(() => {
-    const data = localStorage.getItem("userId")
+    const data = localStorage.getItem("userId");
     if (data && !user) {
-      dispatch(true)
-      if (user) {
-        window.location.reload();
-      }
+      dispatch(true);
     }
-    setusername(localStorage.getItem("userName"))
+    setusername(localStorage.getItem("userName"));
   }, [user]);
+
   return (
-    <div className="top ">
-      <div className="topCenter  ">
-        <div className='w-75 header-first'>
-          <ul className="topList">
-            <li className="topListItem">
-              <Link className="link" style={{ textDecoration: 'none', color: 'black' }} to="/">
-                HOME
-              </Link>
+    <div>
+      <div className="nav-area">
+        <input type="checkbox" id="box" />
+        <label htmlFor="box" className="btn-area" onClick={isNavVisible ? handleToggleNav : () => setIsNavVisible(true)}>
+          <i className={`fa ${isNavVisible ? 'fa-times' : 'fa-bars'}`}></i>
+        </label>
+        <label className="logo h-100">
+        </label>
+        <ul className={isNavVisible ? 'nav-visible' : 'nav-hidden'}>
+          <li>
+            <Link to={"/"} onClick={handleToggleNav}>Home</Link>
+          </li>
+          {user && (
+            <li>
+              <Link to={"/write"} onClick={handleToggleNav}>Write</Link>
             </li>
-            {
-              user && <li className="topListItem">
-                <Link className="link" style={{ textDecoration: 'none', color: 'black' }} to="/write">
-                  WRITE
-                </Link>
+          )}
+          {user && (
+            <li>
+              <Link to={"/myposts"} onClick={handleToggleNav}>My Posts</Link>
+            </li>
+          )}
+          {user ? (
+            <li className='signin-sep-div' onClick={handleLogout}>
+              <a href="/" onClick={handleToggleNav}>Logout</a>
+            </li>
+          ) : (
+            <>
+              <li className='signin-sep-div'>
+                <Link to={"/login"} onClick={handleToggleNav}>Login</Link>
               </li>
-            }
-            {
-              user && <li className="topListItem">
-                <Link className="link" style={{ textDecoration: 'none', color: 'black' }} to="/myposts">
-                  MY POSTS
-                </Link>
+              <li>
+                <Link to={"/register"} onClick={handleToggleNav}>Sign up</Link>
               </li>
-            }
-  
-            {user && <li className="topListItem" onClick={handleLogout} >LOGOUT</li>}
-          </ul>
-        </div>
-      </div>
-      <div className="topRight">
-        {user ? (
-          <Link className="link d-flex" to="">
-            <span className='topListItem'>{username}</span>
-            <img
-              className="topImg"
-              src="https://i.postimg.cc/yNPKjvN9/8380015.jpg"
-              alt=""
-            />
-          </Link>
-        ) : (
-          <ul className="topList">
-            <li className="topListItem">
-              <Link className="link" style={{ textDecoration: 'none', color: 'black' }} to="/login">
-                LOGIN
-              </Link>
-            </li>
-            <li className="topListItem">
-              <Link className="link" style={{ textDecoration: 'none', color: 'black' }} to="/register">
-                REGISTER
-              </Link>
-            </li>
-          </ul>
-        )}
+            </>
+          )}
+        </ul>
       </div>
     </div>
+
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -48,16 +48,47 @@ function Home() {
   return (
     <div>
       {/* top view image in home */}
-      <div className="header">
-        <div className="headerTitles">
-          <span className="headerTitleLg">BLOG</span>
+      <div id="carouselExampleDark" class="carousel carousel-dark slide " data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
-        <img
-          className="headerImg"
-          src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          alt=""
-        />
+        <div class="carousel-inner eh115 ">
+          <div class="carousel-item active " data-bs-interval="3000">
+            <img src="https://i.postimg.cc/1ts2cQR1/woman-with-smartphone-cup-coffee.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block   home-img-1">
+              <p>Discover</p>
+            </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="3000">
+            <img src="https://i.postimg.cc/90FfkKpv/girl-is-reading-book-cafe.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block home-img-2">
+              <p> Learn</p>
+            </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="3000">
+            <img src="https://i.postimg.cc/FKMWDQ9H/education-work-vanlife-concept-cheerful-lovely-redhead-woman-glasses-standing-near-table-read.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block home-img-3">
+              <p>Explore</p>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
+
+
+
+
+
+      
 
       {/* Display current page posts */}
       {currentPosts.map((i) => (
